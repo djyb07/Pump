5bc12d944d2677df92e463d16d8054f590a7b6ed
djyb07 <djyb07@gmail.com>
2026-02-10 23:46:30 +0200

    fix: add self-contained spacer to SmartFilterBar to prevent content overlap

--- client/src/pages/ExerciseLibrary.tsx
+++ client/src/pages/ExerciseLibrary.tsx
@@ -141,8 +141,6 @@ const ExerciseLibrary: React.FC = () => {
                 onClearFilters={clearFilters}
             />
 
-            {/* Spacer for fixed filter bar */}
-            <div className="h-16" />
 
             {/* Main Content */}
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
--- client/src/pages/WorkoutHistoryPage.tsx
+++ client/src/pages/WorkoutHistoryPage.tsx
@@ -252,8 +252,6 @@ export default function WorkoutHistoryPage() {
                 onClearFilters={clearFilters}
             />
 
-            {/* Spacer for fixed filter bar */}
-            <div className="h-16" />
 
             <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
                 {error && (
